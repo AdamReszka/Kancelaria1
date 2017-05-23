883637f2256970454e9c0fd70cae60660283641e
AdamReszka <adam.reszka85@gmail.com>
2017-05-24 01:05:01 +0200

    add parallax effect

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ $(document).ready(initAll);
 
 function initAll(){
   setHeight();
+  startParallax();
 }
 
 function setHeight(){
   $('#sec1-main-cont').height($(window).innerHeight());
+}
+
+function startParallax() {
+  $('#sec1-main-cont').parallax({imageSrc: '../images/notes2.jpg'});
 }
